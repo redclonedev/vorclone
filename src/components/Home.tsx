29efa328a39643f3b0b9b2226ec1caa90c73c307
MaxerRed <65255532+MaxerRed@users.noreply.github.com>
2025-04-27 16:33:13 +0000

    1.2

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,6 @@
 // src/components/Home.tsx
 const Home = () => {
-    return <div>Welcome to Vorclone - Cutting-edge software solutions.</div>
+    return <div>Welcome to test Vorclone - Cutting-edge software solutions.</div>
   }
   
   export default Home
