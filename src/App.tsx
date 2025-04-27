d8e50d9d45c6761c102b965fb30bc61bf2cf53d8
MaxerRed <65255532+MaxerRed@users.noreply.github.com>
2025-04-27 02:10:52 +0000

    1.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,10 +6,6 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-        </a>
-      </div>
       <h1>Vorclone</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
