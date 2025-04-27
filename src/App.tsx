6ef3ac175c4868b24622700c71ebd1901a388857
MaxerRed <65255532+MaxerRed@users.noreply.github.com>
2025-04-27 02:05:29 +0000

    1.1

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,6 @@ function App() {
         <a href="https://vitejs.dev" target="_blank">
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vorclone</h1>
